4e868942e34d3d40c5ac09aa546c2842ca51e6ae
merondagne <siporadagnachew@gmail.com>
2023-05-17 10:55:06 -0700

    done

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
+<<<<<<< HEAD
+
+=======
+>>>>>>> 7e1206470dfea243b5f66f6b6fb583f4e80f4ff0
