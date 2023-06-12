21ecf79bb013df8c322f63fe958c1f33e9f4d32e
merondagne <siporadagnachew@gmail.com>
2023-06-12 11:51:52 -0700

    done

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module implements a custom list object
+"""
+
+
+class MyList(list):
+    """Custom List
+    """
+    def print_sorted(self):
+        """
+        prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
+        
